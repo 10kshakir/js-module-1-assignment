31764f2d48db28d83e906d2efd32c069c36f943a
10kshakir <10kshakir@gmail.com>
2023-03-23 11:29:37 +0600

    assignment done

---
+++ programme.js
@@ -0,0 +1,13 @@
+
+function multiplicationTable(num) {
+      for(let i =1;i<=10;i++){
+            let str =`${num} x ${i} = ${num*i}  `
+            console.log(str);
+      }
+}
+
+multiplicationTable(5);
+console.log("\n");
+multiplicationTable(6);
+console.log("\n");
+multiplicationTable(7);
